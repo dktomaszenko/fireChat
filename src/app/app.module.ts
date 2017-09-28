54c37b438b71d88386ef84afc22d733d09cd1caf
dktomaszenko <dktomaszenko@gmail.com>
2017-09-28 16:59:12 +0200

    AngularFire2 y Firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import {config} from "../environments/firebase.config";
