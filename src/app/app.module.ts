167cdc1f01199262ea58c091cf192de192e832a7
dktomaszenko <dktomaszenko@gmail.com>
2017-09-29 12:46:01 +0200

    modificamos scroll con css webkit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule} from 'angularfire2/database';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {config} from "../environments/firebase.config";
+import {config} from '../environments/firebase.config';
+import { ChatComponent } from './components/chat/chat.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(config),
     AngularFireDatabaseModule
   ],
---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html'
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  enviar(){
+
+  }
+
+}
